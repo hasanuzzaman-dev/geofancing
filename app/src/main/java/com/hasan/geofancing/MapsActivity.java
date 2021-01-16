45a7cf647b4661ae0f60dd244294de2391ceef74
Hasan <hasanuzzaman.dev@gmail.com>
2021-01-16 15:02:25 +0600

    add marker and circle by clicking long

--- app/src/main/java/com/hasan/geofancing/MapsActivity.java
+++ app/src/main/java/com/hasan/geofancing/MapsActivity.java
@@ -9,8 +9,12 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 
 import com.google.android.gms.location.GeofencingClient;
 import com.google.android.gms.location.LocationServices;
@@ -18,15 +22,16 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapLongClickListener {
 
     private static final int FINE_LOCATION_ACCESS_REQUEST_CODE = 1001;
     private GoogleMap mMap;
     private GeofencingClient geofencingClient;
-
+    public static final float GEOFENCE_RADIUS = 200;
 
 
     @Override
@@ -56,19 +61,21 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Add a marker in Sydney and move the camera
         LatLng dhaka = new LatLng(23.7950, 90.3992);
-       // mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(dhaka,16));
+        // mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(dhaka, 16));
 
         enableUserLocation();
+
+        mMap.setOnMapLongClickListener(this);
     }
 
     private void enableUserLocation() {
 
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             mMap.setMyLocationEnabled(true);
-        }else {
+        } else {
             //Ask for permission
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_FINE_LOCATION)){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
                 //We need to show a dialog for displaying why the permission is needed and the ask the permission
                 new AlertDialog.Builder(this)
                         .setMessage("We need to permission for location!")
@@ -80,8 +87,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                             }
                         }).show();
 
-            }else {
-                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},FINE_LOCATION_ACCESS_REQUEST_CODE);
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, FINE_LOCATION_ACCESS_REQUEST_CODE);
             }
         }
 
@@ -90,13 +97,61 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @SuppressLint("MissingPermission")
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode == FINE_LOCATION_ACCESS_REQUEST_CODE){
-            if (grantResults.length >0 && grantResults[0] ==PackageManager.PERMISSION_GRANTED){
+        if (requestCode == FINE_LOCATION_ACCESS_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 // We have the permission
                 mMap.setMyLocationEnabled(true);
-            }else {
-                //We do not have the permission
+            } else {
+                //Permission is not Granted
+                if (!ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                    //This block here means PERMANENTLY DENIED PERMISSION
+                    new androidx.appcompat.app.AlertDialog.Builder(MapsActivity.this)
+                            .setMessage("You have permanently denied this permission, go to settings to enable this permission")
+                            .setPositiveButton("Go to settings", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    gotoApplicationSettings();
+                                }
+                            })
+                            .setNegativeButton("Cancel", null)
+                            .setCancelable(false)
+                            .show();
+                }
             }
         }
+    }
+
+
+    @Override
+    public void onMapLongClick(LatLng latLng) {
+        mMap.clear();
+        addMarker(latLng);
+        addCircle(latLng, GEOFENCE_RADIUS);
+    }
+
+    private void addMarker(LatLng latLng) {
+        MarkerOptions markerOptions = new MarkerOptions().position(latLng);
+        mMap.addMarker(markerOptions);
+    }
+
+    private void addCircle(LatLng latLng, float radius) {
+        CircleOptions circleOptions = new CircleOptions();
+
+        circleOptions.center(latLng)
+                .radius(radius)
+                .strokeColor(Color.argb(255, 255, 0, 0))
+                .fillColor(Color.argb(64, 255, 0, 0))
+                .strokeWidth(4);
+
+        mMap.addCircle(circleOptions);
+    }
+
+
+    private void gotoApplicationSettings() {
+        Intent intent = new Intent();
+        intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        Uri uri = Uri.fromParts("package", this.getPackageName(), null);
+        intent.setData(uri);
+        startActivity(intent);
     }
 }
